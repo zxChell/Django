528d086993f32431cf67dfce3a5fc9386113e4a4
zxChell <144452384+zxChell@users.noreply.github.com>
2023-12-07 14:03:38 +0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from .forms import UserForm
 
 # Create your views here.
 
-def phone(request):
-    return render(request, 'phone.html')
+def index(request):
+    return render (request, 'app/static/templates/login.html')
+
+def postuser(request):
+    
+
